96f57ea2d5dcb30bb0c11db120b354a6a68afe5c
My Doom <alexanderjeanpaul.sm@outlook.com>
2021-11-22 18:33:10 -0500

    Ejercicio_4

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,7 +1,11 @@
-# Ejercicio 3: Escribir un programa que pregunte el
-# nombre del usuario en la consola y despues de que el
-# usuario lo introduzca muestre por pantalla la cadena
-# , donde <nombre> es el nombre que el
-# usuario haya introducido.
+# Ejercicio 4: Escribir un programa que pregunte el nombre del
+# usuario en la consola y un número entero e imprima por pantalla en 
+# líneas distintas el nombre del usuario tantas veces como el número 
+# introducido.
 nombre = input("¿Cual es su nombre?\n")
-print(f"¡Hola  {nombre} !")
+numero = int(input("Ingrese un numero:\n"))
+veces = 0
+while(veces < numero):
+    print(nombre)
+    veces +=1
+
