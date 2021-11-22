317d9c121c4f3219b7d68a101fd409b64d5efeaf
My Doom <alexanderjeanpaul.sm@outlook.com>
2021-11-22 18:08:16 -0500

    Ejercicio_2

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,2 +1,5 @@
-# Ejercicio 1: Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
-print("¡Hola Mundo!")
+# Ejercicio 2: Escribir un programa que almacene la
+# cadena ¡Hola Mundo! en una variable y luego muestre
+# por pantalla el contenido de la variable.
+cadena = "¡Hola Mundo!"
+print(cadena)
